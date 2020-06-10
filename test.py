9c9baad693a1b00a29fc31cc98331ddc7e129cd6
Mjasianek <jasianek.magdalena@gmail.com>
2020-06-10 12:35:21 +0200

    w-statistics and p-value

--- test.py
+++ test.py
@@ -1,14 +1,12 @@
-from strlearn.ensembles import OOB, UOB, SEA, OnlineBagging
-from sklearn.naive_bayes import GaussianNB
-
+import numpy as np
+from scipy.stats import rankdata
+from scipy.stats import ranksums
 from sklearn.base import clone
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import RepeatedStratifiedKFold
-import numpy as np
-
-from scipy.stats import rankdata
-from scipy.stats import ranksums
-
+from sklearn.naive_bayes import GaussianNB
+from strlearn.ensembles import OOB, UOB, SEA, OnlineBagging
+from tabulate import tabulate
 
 #classificators
 clfs = {   
@@ -19,7 +17,17 @@ clfs = {
 }
 
 #our_data_sets
-datasets = ['australian', 'breastcan', 'cryotherapy','diabetes','german','liver','heart','ionosphere', 'monkone', 'wisconsin']
+datasets = ['australian',
+            'breastcan',
+            'cryotherapy',
+            'diabetes',
+            'german',
+            'liver',
+            'heart',
+            'ionosphere',
+            'monkone',
+            'wisconsin'
+            ]
 
 #experiment
 n_datasets = len(datasets)    #length of dict
@@ -66,3 +74,40 @@ print("\nRanks:\n", ranks)
 #mean ranks
 mean_ranks = np.mean(ranks, axis=0)
 print("\nMean ranks:\n", mean_ranks)
+
+# w-statistic and p-value
+alfa = 0.05
+w_statistic = np.zeros((len(clfs), len(clfs)))
+p_value = np.zeros((len(clfs), len(clfs)))
+
+for i in range(len(clfs)):
+    for j in range(len(clfs)):
+        w_statistic[i, j], p_value[i, j] = ranksums(ranks.T[i], ranks.T[j])
+
+headers = list(clfs.keys())
+names_column = np.expand_dims(np.array(list(clfs.keys())), axis=1)
+w_statistic_table = np.concatenate((names_column, w_statistic), axis=1)
+w_statistic_table = tabulate(w_statistic_table, headers, floatfmt=".2f")
+p_value_table = np.concatenate((names_column, p_value), axis=1)
+p_value_table = tabulate(p_value_table, headers, floatfmt=".2f")
+print("\nw-statistic:\n", w_statistic_table, "\n\np-value:\n", p_value_table)
+
+# Advantage
+advantage = np.zeros((len(clfs), len(clfs)))
+advantage[w_statistic > 0] = 1
+advantage_table = tabulate(np.concatenate(
+    (names_column, advantage), axis=1), headers)
+print("\nAdvantage:\n", advantage_table)
+
+# Statistical significance
+significance = np.zeros((len(clfs), len(clfs)))
+significance[p_value <= alfa] = 1
+significance_table = tabulate(np.concatenate(
+    (names_column, significance), axis=1), headers)
+print("\nStatistical significance (alpha = 0.05):\n", significance_table)
+
+# Statistical significance better
+stat_better = significance * advantage
+stat_better_table = tabulate(np.concatenate(
+    (names_column, stat_better), axis=1), headers)
+print("Statistically significantly better:\n", stat_better_table)
