67508537ea2b0ef212fd7e75b79611a9a051b298
Konrad Prusak <61755751+konradprusak@users.noreply.github.com>
2020-06-22 18:59:06 +0200

    generate streams and data analysis

--- test.py
+++ test.py
@@ -1,4 +1,3 @@
-import numpy as np
 from scipy.stats import rankdata
 from scipy.stats import ranksums
 from sklearn.base import clone
@@ -8,62 +7,154 @@ from sklearn.naive_bayes import GaussianNB
 from strlearn.ensembles import OOB, UOB, SEA, OnlineBagging
 from tabulate import tabulate
 
+import numpy as np
+import strlearn as sl
+from sklearn.neural_network import MLPClassifier
+import matplotlib.pyplot as plt
+
+
+
+########### GENERATING STREAMS #######################################
+
+##generate sudden drift
+stream_sudden = sl.streams.StreamGenerator(n_chunks=200,
+                                            chunk_size=500,
+                                            n_classes=2,
+                                            n_drifts=1,
+                                            n_features=10,
+                                            random_state=10)
+
+#generatre gradual drift
+stream_gradual = sl.streams.StreamGenerator(n_chunks=200,
+                                            chunk_size=500,
+                                            n_classes=2,
+                                            n_drifts=1,
+                                            n_features=10,
+                                            random_state=10,
+                                            concept_sigmoid_spacing=5)
+
+#generate incremental drift
+stream_incremental = sl.streams.StreamGenerator(n_chunks=200,
+                                                chunk_size=500,
+                                                n_classes=2,
+                                                n_drifts=1,
+                                                n_features=10,
+                                                random_state=10,
+                                                concept_sigmoid_spacing=5,
+                                                incremental=True)
+
+######################################################################
+
+
+
 #classificators
 clfs = {   
     'UOB': UOB(base_estimator=GaussianNB(), n_estimators=5),
     'OOB': OOB(base_estimator=GaussianNB(), n_estimators=5),
-    'OB' : OnlineBagging(base_estimator=GaussianNB(), n_estimators=5),
+    'OB': OnlineBagging(base_estimator=GaussianNB(), n_estimators=5),
     'SEA': SEA(base_estimator=GaussianNB(), n_estimators=5)
 }
 
-#our_data_sets
-datasets = ['australian',
-            'breastcan',
-            'cryotherapy',
-            'diabetes',
-            'german',
-            'liver',
-            'heart',
-            'ionosphere',
-            'monkone',
-            'wisconsin'
-            ]
 
-#experiment
-n_datasets = len(datasets)    #length of dict
-n_splits = 5                  #number of splits
-n_repeats = 2
-rskf = RepeatedStratifiedKFold(n_splits=n_splits, n_repeats=n_repeats, random_state=1234)
 
-scores = np.zeros((len(clfs), n_datasets, n_splits * n_repeats))
 
-#reading_data_input 
-for data_id, dataset in enumerate(datasets):
-    dataset = np.genfromtxt("datasets/%s.csv" % (dataset), delimiter=",")
-    X = dataset[:, :-1]
-    y = dataset[:, -1].astype(int)
+#chosen metrics
+metrics = [sl.metrics.f1_score,
+           sl.metrics.geometric_mean_score_1]
 
-    for fold_id, (train, test) in enumerate(rskf.split(X, y)):
-        for clf_id, clf_name in enumerate(clfs):
-            clf = clone(clfs[clf_name])
-            clf.fit(X[train], y[train])
-            y_pred = clf.predict(X[test])
-            scores[clf_id, data_id, fold_id] = accuracy_score(y[test], y_pred)
+#metrics names
+metrics_names = ["F1 score",
+                 "G-mean"]
+
+
+
+
+#evaluator initialization
+evaluator_sudden = sl.evaluators.TestThenTrain(metrics)
+evaluator_gradual = sl.evaluators.TestThenTrain(metrics)
+evaluator_incremental = sl.evaluators.TestThenTrain(metrics)
+
+#evaluator run
+evaluator_sudden.process(stream_sudden, clfs.get('UOB'))
+evaluator_gradual.process(stream_gradual, clfs.get('UOB'))
+evaluator_incremental.process(stream_incremental, clfs.get('UOB')) 
+
+"""
+for i in clfs.values(): 
+    evaluator_sudden.process(stream_sudden, i)
+
+for i in clfs.values(): 
+    evaluator_gradual.process(stream_gradual, i)
+
+for i in clfs.values(): 
+    evaluator_incremental.process(stream_incremental, i)
+"""
+
+#print scores results
+#print(evaluator_sudden.scores)
+
+
+
 
 #saving_results_to_file
-np.save('results', scores)
+def save_to_file(evaluator, drift_name:str):
+    np.save('results_' + drift_name, evaluator.scores)
+
+save_to_file(evaluator_sudden, "sudden")
+save_to_file(evaluator_gradual, "gradual")
+save_to_file(evaluator_incremental, "incremental")
 
 
-### DATA ANALYSIS ###
+
+
+
+############# DATA ANALYSIS ######################################
 
 #reading_result_from_file 
-scores = np.load('results.npy')
-print("\nScores:\n", scores.shape)
+scores_sudden = np.load('results_sudden.npy')
+print("\nScores (sudden):\n", scores_sudden.shape)
+
+scores_gradual = np.load('results_gradual.npy')
+print("\nScores (gradual):\n", scores_gradual.shape)
+
+scores_incremental = np.load('results_incremental.npy')
+print("\nScores (incremental):\n", scores_incremental.shape)
+
 
 #mean scores 
-mean_scores = np.mean(scores, axis=2).T
-print("\nMean scores:\n", mean_scores)
+mean_scores_sudden = np.mean(scores_sudden, axis=2).T
+print("\nMean scores (sudden):\n", mean_scores_sudden)
 
+mean_scores_gradual = np.mean(scores_gradual, axis=2).T
+print("\nMean scores (gradual):\n", mean_scores_gradual)
+
+mean_scores_incremental = np.mean(scores_incremental, axis=2).T
+print("\nMean scores (incremental):\n", mean_scores_incremental)
+
+#std
+std_scores_sudden = np.std(scores_sudden, axis=2).T
+print("\nStd scores (sudden):\n", std_scores_sudden)
+
+std_scores_gradual = np.std(scores_gradual, axis=2).T
+print("\nStd scores (gradual):\n", std_scores_gradual)
+
+std_scores_incremental = np.std(scores_incremental, axis=2).T
+print("\nStd scores (incremental):\n", std_scores_incremental)
+
+
+def show_results(mean, std): 
+    for clf_id, clf_name in enumerate(clfs):
+        print("%s: %.3f (%.2f)" % (clf_name, mean[clf_id], std[clf_id]))
+
+show_results(mean_scores_sudden, std_scores_sudden)
+
+###############################################################################3
+
+
+
+
+
+"""
 #ranks
 ranks = []
 for ms in mean_scores:
@@ -111,3 +202,4 @@ stat_better = significance * advantage
 stat_better_table = tabulate(np.concatenate(
     (names_column, stat_better), axis=1), headers)
 print("Statistically significantly better:\n", stat_better_table)
+"""
