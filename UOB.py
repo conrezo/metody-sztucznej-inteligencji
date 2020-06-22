af797da71ac75afd11937c110bea6af1ba5acecc
Bartlomiej Swoboda <bartlomiejswoboda@gmail.com>
2020-06-22 03:54:31 -0700

    code analysis

--- UOB.py
+++ UOB.py
@@ -14,18 +14,22 @@ class UOB(BaseEnsemble, ClassifierMixin):
     """
     Undersampling-Based Online Bagging.
     """
-
+    #funkcja init 
+    #time_decay_factor = 0.9
+    #base_estmator ???
+    #n_estimators = 5 ???
+    #input: zbiory Yminority, Ymajority z M uczacymi
     def __init__(self, base_estimator=None, n_estimators=5, time_decay_factor=0.9):
         """Initialization."""
         self.base_estimator = base_estimator
         self.n_estimators = n_estimators
         self.time_decay_factor = time_decay_factor
-
+    #funkcja odpowiedzialna za przygotowanie i dopasowanie danych
     def fit(self, X, y):
         """Fitting."""
         self.partial_fit(X, y)
         return self
-
+    #dopasowanie oraz klonowanie z uzwglednieniem odpowiedniego estymatora
     def partial_fit(self, X, y, classes=None):
         """Partial fitting."""
         X, y = check_X_y(X, y)
@@ -41,53 +45,56 @@ class UOB(BaseEnsemble, ClassifierMixin):
         self.X_, self.y_ = X, y
 
         # Check classes
+        # sprawdzanie czy klasa instnieje oraz zwracanie unikalnych etykiet w inwersji
         self.classes_ = classes
         if self.classes_ is None:
             self.classes_, _ = np.unique(y, return_inverse=True)
 
         # time decayed class sizes tracking
         if not hasattr(self, "last_instance_sizes"):
-            self.current_tdcs_ = np.zeros((1, 2))
+            self.current_tdcs_ = np.zeros((1, 2))    #gdy nie ma tdcs uzupelnianie zerami 
         else:
-            self.current_ctdcs_ = self.last_instance_sizes
+            self.current_ctdcs_ = self.last_instance_sizes #w innym przypadku przypisanie do obecnego tdcs ostatniego tdcs
 
         self.chunk_tdcs = np.ones((self.X_.shape[0], self.classes_.shape[0]))
-
+        #okreslanie rozmiaru klasy dla zboriu etykiet 
         for iteration, label in enumerate(self.y_):
-            if label == 0:
+            if label == 0: #????
                 self.current_tdcs_[0, 0] = (
                     self.current_tdcs_[0, 0] * self.time_decay_factor
-                ) + (1 - self.time_decay_factor)
+                ) + (1 - self.time_decay_factor)  #etykieta xt jest ck
                 self.current_tdcs_[0, 1] = (
                     self.current_tdcs_[0, 1] * self.time_decay_factor
-                )
+                ) #etykieta xt nie jest ck ???
             else:
                 self.current_tdcs_[0, 1] = (
                     self.current_tdcs_[0, 1] * self.time_decay_factor
-                ) + (1 - self.time_decay_factor)
+                ) + (1 - self.time_decay_factor) #etykieta xt jest ck
                 self.current_tdcs_[0, 0] = (
                     self.current_tdcs_[0, 0] * self.time_decay_factor
-                )
+                ) #etykita xt nie jest ck
 
-            self.chunk_tdcs[iteration] = self.current_tdcs_
+            self.chunk_tdcs[iteration] = self.current_tdcs_ #dla kazdej iteracji M klas wejsciowych przypisanie obecnego tdcs 
 
-        self.last_instance_sizes = self.current_tdcs_
+        self.last_instance_sizes = self.current_tdcs_ #aktualizacja rozmiaru klasy w kazdej iteracji
 
         # improved UOB
-        self.weights = []
+        # assumptions: Y(-) -> klasa negatywna, Y(+) -> klasa pozytywna 
+        # w+/ chuck_tdcs [instance][1] , w-/ chuck_tdcs [instance][0]
+        self.weights = [] #tablica wag
         for instance, label in enumerate(self.y_):
             if (
-                label == 1
-                and self.chunk_tdcs[instance][1] > self.chunk_tdcs[instance][0]
+                label == 1 #klasa pozytywna???
+                and self.chunk_tdcs[instance][1] > self.chunk_tdcs[instance][0] # w(+)/w(-)
             ):
                 lmbda = self.chunk_tdcs[instance][0] / \
-                    self.chunk_tdcs[instance][1]
+                    self.chunk_tdcs[instance][1]        # obliczanie wartosci lambda do poissona 
                 K = np.asarray(
                     [np.random.poisson(lmbda, 1)[0]
-                     for i in range(self.n_estimators)]
+                     for i in range(self.n_estimators)] #wyznaczanie wartosci k 
                 )
             elif (
-                label == 0
+                label == 0 #klasa negatywna 
                 and self.chunk_tdcs[instance][0] > self.chunk_tdcs[instance][1]
             ):
                 lmbda = self.chunk_tdcs[instance][1] / \
@@ -96,15 +103,15 @@ class UOB(BaseEnsemble, ClassifierMixin):
                     [np.random.poisson(lmbda, 1)[0]
                      for i in range(self.n_estimators)]
                 )
-            else:
+            else: #jezeli lambda rowna jeden
                 lmbda = 1
                 K = np.asarray(
                     [np.random.poisson(lmbda, 1)[0]
                      for i in range(self.n_estimators)]
                 )
-            self.weights.append(K)
+            self.weights.append(K) #rozszerzanie tablicy wag
 
-        self.weights = np.asarray(self.weights).T
+        self.weights = np.asarray(self.weights).T #formatowanie tablicy 
 
         for w, base_model in enumerate(self.ensemble_):
             base_model.partial_fit(
@@ -138,9 +145,9 @@ class UOB(BaseEnsemble, ClassifierMixin):
         if X.shape[1] != self.X_.shape[1]:
             raise ValueError("number of features does not match")
 
-        esm = self.ensemble_support_matrix(X)
-        average_support = np.mean(esm, axis=0)
-        prediction = np.argmax(average_support, axis=1)
+        esm = self.ensemble_support_matrix(X) #macierz wsparcia 
+        average_support = np.mean(esm, axis=0) #wsparcie wartosci sredniej
+        prediction = np.argmax(average_support, axis=1) #wyznaczenie wartosci maksymalnej predykcji 
 
         # Return prediction
-        return self.classes_[prediction]
+        return self.classes_[prediction] #zwrocenie wyznaczonej predykcji 
