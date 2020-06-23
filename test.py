3f4f1c9b9a59d2ca4cbd2e0b590d0823cbcf058a
Konrad Prusak <61755751+konradprusak@users.noreply.github.com>
2020-06-23 16:04:09 +0200

    add statistical analysys (not working yet)

--- test.py
+++ test.py
@@ -1,20 +1,32 @@
-#from scipy.stats import rankdata
-#from scipy.stats import ranksums
-from sklearn.base import clone
-from sklearn.metrics import accuracy_score
-from sklearn.model_selection import RepeatedStratifiedKFold
-from sklearn.naive_bayes import GaussianNB
 from strlearn.ensembles import OOB, UOB, SEA, OnlineBagging
-from tabulate import tabulate
-
+from sklearn.naive_bayes import GaussianNB
 import numpy as np
 import strlearn as sl
 from sklearn.neural_network import MLPClassifier
 import matplotlib.pyplot as plt
+from scipy.stats import ttest_ind
+from tabulate import tabulate
+
+#classificators
+clfs = {   
+    'UOB': UOB(base_estimator=GaussianNB(), n_estimators=5),
+    'OOB': OOB(base_estimator=GaussianNB(), n_estimators=5),
+    'OB': OnlineBagging(base_estimator=GaussianNB(), n_estimators=5),
+    'SEA': SEA(base_estimator=GaussianNB(), n_estimators=5)
+}
+
+
+#metrics
+metrics = [sl.metrics.f1_score,
+           sl.metrics.geometric_mean_score_1]
+
+#metrics names
+metrics_names = ["F1 score",
+                 "G-mean"]
 
 
 
-########### GENERATING STREAMS #######################################
+########### GENERATING STREAMS #######################################################
 
 ##generate sudden drift
 stream_sudden = sl.streams.StreamGenerator(n_chunks=200,
@@ -43,29 +55,9 @@ stream_incremental = sl.streams.StreamGenerator(n_chunks=200,
                                                 concept_sigmoid_spacing=5,
                                                 incremental=True)
 
-######################################################################
-
-
-#classificators
-clfs = {   
-    'UOB': UOB(base_estimator=GaussianNB(), n_estimators=5),
-    'OOB': OOB(base_estimator=GaussianNB(), n_estimators=5),
-    'OB': OnlineBagging(base_estimator=GaussianNB(), n_estimators=5),
-    'SEA': SEA(base_estimator=GaussianNB(), n_estimators=5)
-}
-
-
-#metrics
-metrics = [sl.metrics.f1_score,
-           sl.metrics.geometric_mean_score_1]
-
-#metrics names
-metrics_names = ["F1 score",
-                 "G-mean"]
-
 
 #evaluator initialization
-#czy powinniśmy to przepuścić przez wszystkie metryki i wszystkie clfs? mnóstwo przypadków
+#czy powinniśmy to przepuścić przez wszystkie metryki i wszystkie clfs? mnóstwo przypadków...
 
 evaluator_sudden = sl.evaluators.TestThenTrain(metrics)
 evaluator_gradual = sl.evaluators.TestThenTrain(metrics)
@@ -88,8 +80,7 @@ evaluator_incremental_OB = sl.evaluators.TestThenTrain(metrics)
 evaluator_incremental_SEA = sl.evaluators.TestThenTrain(metrics)
 """
 
-#evaluators run
-
+#run evaluators
 evaluator_sudden.process(stream_sudden, clfs.values())
 evaluator_gradual.process(stream_gradual, clfs.values())
 evaluator_incremental.process(stream_incremental, clfs.values())
@@ -111,22 +102,10 @@ evaluator_incremental_OB.process(stream_incremental, clfs.get('OB'))
 evaluator_incremental_SEA.process(stream_incremental, clfs.get('SEA')) 
 """
 
-""" 
-for i in clfs.values(): 
-    evaluator_sudden.process(stream_sudden, i)
-
-for i in clfs.values(): 
-    evaluator_gradual.process(stream_gradual, i)
-
-for i in clfs.values(): 
-    evaluator_incremental.process(stream_incremental, i)
-"""
-
-
 #print scores results
 #print(evaluator_sudden.scores)
 
-#saving_results_to_file
+#saving results (evaluator scores) to file
 def save_to_file(evaluator, drift_name:str):
     np.save('results_' + drift_name, evaluator.scores)
 
@@ -135,7 +114,8 @@ save_to_file(evaluator_gradual, "gradual")
 save_to_file(evaluator_incremental, "incremental")
 
 
-############# DATA ANALYSIS ######################################
+
+################ DATA ANALYSIS ############################################################
 
 #reading_result_from_file 
 scores_sudden = np.load('results_sudden.npy')
@@ -170,6 +150,7 @@ std_scores_incremental = np.std(scores_incremental)
 print("\nStd scores (incremental):\n", std_scores_incremental)
 
 
+
 ############## PRESENTING RESULTS ######################################################
 
 def show_results(mean, std): 
@@ -183,56 +164,57 @@ show_results(mean_scores_gradual, std_scores_gradual)
 print("incremental")
 show_results(mean_scores_incremental, std_scores_incremental)
 
-########################################################################################
-
 
 
 """
-#ranks
-ranks = []
-for ms in mean_scores:
-    ranks.append(rankdata(ms).tolist())
-ranks = np.array(ranks)
-print("\nRanks:\n", ranks)
+############# STATISTICAL ANALYSIS #################################################################
 
-#mean ranks
-mean_ranks = np.mean(ranks, axis=0)
-print("\nMean ranks:\n", mean_ranks)
-
-# w-statistic and p-value
-alfa = 0.05
-w_statistic = np.zeros((len(clfs), len(clfs)))
+alfa = .05
+t_statistic = np.zeros((len(clfs), len(clfs)))
 p_value = np.zeros((len(clfs), len(clfs)))
 
-for i in range(len(clfs)):
-    for j in range(len(clfs)):
-        w_statistic[i, j], p_value[i, j] = ranksums(ranks.T[i], ranks.T[j])
+#creating t_statistic and p_value matrices
+def get_t_statistics_p_value(scores):
+    for i in range(len(clfs)):
+        for j in range(len(clfs)):
+            t_statistic[i, j], p_value[i, j] = ttest_ind(scores[i], scores[j])
+    print("t-statistic:\n", t_statistic, "\n\np-value:\n", p_value)
 
-headers = list(clfs.keys())
-names_column = np.expand_dims(np.array(list(clfs.keys())), axis=1)
-w_statistic_table = np.concatenate((names_column, w_statistic), axis=1)
-w_statistic_table = tabulate(w_statistic_table, headers, floatfmt=".2f")
+print("Scores sudden \n")
+get_t_statistics_p_value(scores_sudden)
+print("Scores gradual \n")
+get_t_statistics_p_value(scores_gradual)
+print("Scores incremental \n")
+get_t_statistics_p_value(scores_incremental)
+
+headers = ["GNB", "kNN", "CART"]
+names_column = np.array([["GNB"], ["kNN"], ["CART"]])
+t_statistic_table = np.concatenate((names_column, t_statistic), axis=1)
+t_statistic_table = tabulate(t_statistic_table, headers, floatfmt=".2f")
 p_value_table = np.concatenate((names_column, p_value), axis=1)
 p_value_table = tabulate(p_value_table, headers, floatfmt=".2f")
-print("\nw-statistic:\n", w_statistic_table, "\n\np-value:\n", p_value_table)
+print("t-statistic:\n", t_statistic_table, "\n\np-value:\n", p_value_table)
 
-# Advantage
+
+#advantage matrics
 advantage = np.zeros((len(clfs), len(clfs)))
-advantage[w_statistic > 0] = 1
+advantage[t_statistic > 0] = 1
 advantage_table = tabulate(np.concatenate(
     (names_column, advantage), axis=1), headers)
-print("\nAdvantage:\n", advantage_table)
+print("Advantage:\n", advantage_table)
 
-# Statistical significance
+
+#significance table
 significance = np.zeros((len(clfs), len(clfs)))
 significance[p_value <= alfa] = 1
 significance_table = tabulate(np.concatenate(
     (names_column, significance), axis=1), headers)
-print("\nStatistical significance (alpha = 0.05):\n", significance_table)
+print("Statistical significance (alpha = 0.05):\n", significance_table)
 
-# Statistical significance better
+#final results
 stat_better = significance * advantage
 stat_better_table = tabulate(np.concatenate(
     (names_column, stat_better), axis=1), headers)
 print("Statistically significantly better:\n", stat_better_table)
+
 """
